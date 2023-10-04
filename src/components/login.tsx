19dcdc6ebcfca7ddb2400b44846cd7cb1fb5aae9
Huy, Vuong Do Thanh <vuongdothanhhuy@gmail.com>
2023-10-04 18:39:10 +0700

    Add Signup feature.

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -40,12 +40,40 @@ const Login = ({setCurrUser, setIsAuth}: LoginProps) => {
             setCurrUser(reqValue)
             setIsAuth(true)
         } else {
-            alert('Cannot login/register!')
+            alert('Cannot login!')
+        }
+    }
+
+    // sign up for new account
+    const signup = async () => {
+        const req = await fetch('/api/signup', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({email, password})
+        })
+        const reqValue = await req.json()
+
+        if (req.ok) {
+            /**
+             * To keep everything simple in a constraint timeline, some decisions have been made:
+             *  - When the API return ok, it means account is registered/login, and session is generated on BE.
+             *  - On FE, we store the whole returned User obj to be used later. In real life, this is discouraged,
+             *      and it must be implemented in a more properly way.
+             *  - We have an "isAuth" bool var to tell whether we're logged in or not. In real life,
+             *      it can be an SWR that constantly fetch status from the server, better!
+             */
+            setCurrUser(reqValue)
+            setIsAuth(true)
+        } else {
+            alert('Cannot signup!')
         }
     }
 
     return (
-        <div className="login-form-wrapper">
+        <div className="login-form-wrapper" data-testid="login-component">
             <div className="flex-1 flex flex-col md:flex-row gap-4 md:gap-2">
                 <div className="w-full flex-1">
                     <div className="bg-white h-full flex border border-gray-200 rounded">
@@ -67,9 +95,16 @@ const Login = ({setCurrUser, setIsAuth}: LoginProps) => {
                 </div>
                 <div className="w-max-fit">
                     <button type="button"
+                            data-testid="login-button"
                             onClick={login}
                             className="p-1 px-2 bg-blue-500 w-full text-gray-800 border border-gray-200 rounded hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm sm:w-auto py-2.5 text-center">
-                        Login/Register
+                        Login
+                    </button>
+                    <button
+                        data-testid="signup-button"
+                        onClick={signup}
+                        className="p-1 px-2 bg-amber-600 w-full text-gray-800 border border-gray-200 rounded hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium text-sm sm:w-auto py-2.5 text-center">
+                        Register
                     </button>
                 </div>
             </div>
---
+++ src/pages/api/signup.ts
@@ -0,0 +1,45 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import {NextApiRequest, NextApiResponse} from "next";
+import {setCookie} from "cookies-next";
+import crypto from "crypto";
+import prisma from "@/helper/prisma";
+
+export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse
+) {
+    if (req.method === 'POST') {
+        // Process a POST request
+        const {email, password} = req.body
+        if (email && password) {
+            let hashPwd = crypto.createHash('md5').update(password).digest('hex');
+
+            const result = await prisma.user.upsert({
+                where: {
+                    email: email,
+                },
+                update: {},
+                create: {
+                    email: email,
+                    password: hashPwd
+                }
+            })
+            if (result) {
+                // signup ok
+                // For simplicity, we use the whole User object as the token. Bad practice in real life,
+                //  use something like JWT for this.
+                setCookie('login_token', result, {
+                    req,
+                    res,
+                    maxAge: 60 * 60 * 24,
+                    httpOnly: true,
+                    path: '/'
+                })
+
+                return res.status(201).json(result)
+            }
+            return res.status(403).json({})
+        }
+    }
+    return res.status(400).json({})
+}
