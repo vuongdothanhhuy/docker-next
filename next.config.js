36ff2f0ac95d05c1a52fbd11d44c023c1d7c4b23
Huy, Vuong Do Thanh <vuongdothanhhuy@gmail.com>
2023-10-04 18:40:17 +0700

    Add whitelist for Next Image

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+    reactStrictMode: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'img.youtube.com',
+            },
+        ]
+    }
 }
 
 module.exports = nextConfig
