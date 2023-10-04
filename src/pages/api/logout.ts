b6659d42aa19ce1dba8c3a53e2701062c7529678
Huy, Vuong Do Thanh <vuongdothanhhuy@gmail.com>
2023-10-04 20:18:06 +0700

    Fix bug cannot logout and clear session

--- src/pages/api/logout.ts
+++ src/pages/api/logout.ts
@@ -3,9 +3,9 @@ import {NextApiRequest, NextApiResponse} from "next";
 import {deleteCookie} from "cookies-next";
 
 export default async function handler(
-    _req: NextApiRequest,
+    req: NextApiRequest,
     res: NextApiResponse
 ) {
-    deleteCookie('login_token')
+    deleteCookie('login_token', {req, res, path: '/'})
     return res.status(200).json({})
 }
