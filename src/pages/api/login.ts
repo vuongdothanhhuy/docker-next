d06e695aa8f37040a6ee2c188c7f6717557a5804
Huy, Vuong Do Thanh <vuongdothanhhuy@gmail.com>
2023-10-06 18:03:21 +0700

    Add BE test for login API

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -1,22 +1,22 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import {NextApiRequest, NextApiResponse} from "next";
-import {setCookie} from "cookies-next";
-import crypto from "crypto";
-import prisma from "@/helper/prisma";
+import {setCookie}                       from "cookies-next";
+import crypto                            from "crypto";
+import prisma                            from "@/helper/prisma";
 
 export default async function handler(
     req: NextApiRequest,
-    res: NextApiResponse
+    res: NextApiResponse,
 ) {
     if (req.method === 'POST') {
         // Process a POST request
         const {email, password} = req.body
         if (email && password) {
             const result = await prisma.user.findFirst({
-                where: {
-                    email: email,
-                }
-            })
+                                                           where: {
+                                                               email: email,
+                                                           },
+                                                       })
             let hashPwd = crypto.createHash('md5').update(password).digest('hex');
             if (result?.password === hashPwd) {
                 //login ok
@@ -27,7 +27,7 @@ export default async function handler(
                     res,
                     maxAge: 60 * 60 * 24,
                     httpOnly: true,
-                    path: '/'
+                    path: '/',
                 })
 
                 return res.status(201).json(result)
